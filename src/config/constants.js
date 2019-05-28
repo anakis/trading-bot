2299ae30a19643668ec2ccc0a757facc7694fa1e
Thiago <thiag.g3@gmail.com>
2019-05-28 17:55:21 -0300

    refact: add dotenv-parse-variables to parse env automagic

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,35 +1,4 @@
-require('dotenv').config()
+const env = require('dotenv').config()
+const dotenvParseVariables = require('dotenv-parse-variables')
 
-const getBases = () => process.env.BASE_ARRAY.split(',')
-
-const getQuote = () => process.env.QUOTE_COIN
-
-const getTimeFrame = () => process.env.TIMEFRAME
-
-const getRSIperiod = () => parseInt(process.env.RSI_PERIOD, 10)
-
-const getStochasticPeriod = () => parseInt(process.env.STOCHASTIC_PERIOD, 10)
-
-const getStochasticSignalPeriod = () => parseInt(process.env.STOCHASTIC_SIGNAL_PERIOD, 10)
-
-const getATRperiod = () => parseInt(process.env.ATR_PERIOD, 10)
-
-module.exports = () => {
-  const bases = getBases()
-  const quote = getQuote()
-  const timeframe = getTimeFrame()
-  const rsiPeriod = getRSIperiod()
-  const stochasticPeriod = getStochasticPeriod()
-  const stochasticSignalPeriod = getStochasticSignalPeriod()
-  const atrPeriod = getATRperiod()
-
-  return {
-    bases,
-    quote,
-    timeframe,
-    rsiPeriod,
-    stochasticPeriod,
-    stochasticSignalPeriod,
-    atrPeriod,
-  }
-}
+module.exports = () => dotenvParseVariables(env.parsed)
