561f4415dc2c821d82933c4a72a26e372d068f21
Thiago <thiag.g3@gmail.com>
2019-08-07 15:39:50 -0300

    feat: add ORDER_TIMEOUT env and ORDER_CHECKS. add stopLoss amount on Long positions to be coin balance

--- src/module/ordersMonitor.js
+++ src/module/ordersMonitor.js
@@ -4,8 +4,8 @@ const sleep = require('sleep-promise')
 
 module.exports = app => {
   const checkOrder = async ({ order, orders, checks }) => {
-    await sleep(10000)
-    if (checks === 5) {
+    await sleep(this.check_timeout)
+    if (checks === this.check) {
       console.log(`${order.id} order cancelled by timeout`)
       return this.removeOrder(order.id)
     }
@@ -23,12 +23,24 @@ module.exports = app => {
       type: action,
     })
     console.log(` ${order.id} order executed! Adding stop loss.`)
-    this.createStopLoss({
-      symbol,
-      amount,
-      side,
-      price: stopLoss,
-    })
+    if (action === 'LONG') {
+      const pair = this.pairs.find(p => p.symbol === symbol)
+      const balance = await this.loadBalance()
+
+      this.createStopLoss({
+        symbol,
+        amount: balance[pair.base],
+        side,
+        price: stopLoss,
+      })
+    } else {
+      this.createStopLoss({
+        symbol,
+        amount,
+        side,
+        price: stopLoss,
+      })
+    }
     return {}
   }
 
@@ -41,12 +53,21 @@ module.exports = app => {
   }
 
   const init = async () => {
-    const { getOpenOrders, removeOrder, createStopLoss } = await app.module.dataGateway
+    const {
+      getOpenOrders, removeOrder, loadBalance, getPairs, createStopLoss,
+    } = await app.module
+      .dataGateway
+
+    const { ORDER_TIMEOUT, ORDER_CHECKS } = app.config.constants
     const { savePosition } = app.module.positionManager
     this.getOpenOrders = getOpenOrders
     this.removeOrder = removeOrder
     this.createStopLoss = createStopLoss
     this.savePosition = savePosition
+    this.check_timeout = ORDER_TIMEOUT / ORDER_CHECKS
+    this.check = ORDER_CHECKS
+    this.pairs = getPairs()
+    this.loadBalance = loadBalance
   }
 
   init()
